c6e91dc66e69a2da69df6b738f7704c6aac6cddf
Nodar Chabashvili <dachi1014@gmail.com>
2022-11-10 22:58:45 +0100

    Added register for admins

--- input/admins.php
+++ input/admins.php
@@ -1,19 +1,9 @@
 <?php
 include_once(__DIR__ . '/../app/start_connect.php');
 ?>
-<h2>Insert a new User</h2>
+<h2>Insert a new Admin</h2>
 <a href="<?=ROOT?>reference/customer.php" class="reference">Reference</a>
 <form method='POST' action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <label for="firstname">Firstname:</label>
-    <input type="text" name="firstname" id="firstname" required/><span class="must">*</span>
-    <br/>
-
-    <label for="lastname">Lastname:</label>
-    <input type="text" name="lastname" id="lastname" required/><span class="must">*</span>
-    <br/>
-
-    <label for="email">Email:</label>
-    <input type="email" name="email" id="email" required/><span class="must">*</span>
 
     <label for="username">Username:</label>
     <input type="text" name="username" required></input>
@@ -44,15 +34,8 @@ include_once(__DIR__ . '/../app/start_connect.php');
 
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
-
-    include_once(__DIR__ . '/../app/auth.php');
-    passAuth($username, $password, $conn);
-
-    $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
-    $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
         
-    $sql = "INSERT INTO Users (firstname, lastname, email) VALUES ('$firstname', '$lastname', '$email')";
+    $sql = "INSERT INTO Admins (username, password) VALUES ('$username', '$password')";
     if (!$conn->query($sql)) {
         echo "Insert error: " . mysqli_error($conn);
         mysqli_close($conn);
